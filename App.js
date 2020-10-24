f3c0f4dbaa17c6b01661b3e123c819a246ee6101
Sam Friggens <sam.friggens@adi.do>
2020-10-24 13:48:55 +0100

    added status bar constraint

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import Constants from "expo-constants";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={styles.statusBar} />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -12,10 +14,14 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  statusBar: {
+    // backgroundColor: "blue",
+    height: Constants.statusBarHeight
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
