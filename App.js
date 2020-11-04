e13091a4ed100a775972def54f1ed81b1bb03bfc
SamFriggs111 <45073736+SamFriggs111@users.noreply.github.com>
2020-11-04 23:07:10 +0000

    New home started

--- App.js
+++ App.js
@@ -1,113 +1,101 @@
-import "react-native-gesture-handler";
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { MaterialCommunityIcons, MaterialIcons, Feather, FontAwesome } from '@expo/vector-icons';
 
-import * as React from "react";
-import { Button, View, Text, TouchableOpacity, Image } from "react-native";
-
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-
-import LandingPage from "./components/LandingPage";
-import MapsView from "./components/MapsView";
-import FaqPage from "./components/FaqPage";
-
-const Stack = createStackNavigator();
-const Drawer = createDrawerNavigator();
-
-const NavigationDrawerStructure = props => {
-  const toggleDrawer = () => {
-    props.navigationProps.toggleDrawer();
-  };
-
+function Feed() {
   return (
-    <View style={{ flexDirection: "row" }}>
-      <TouchableOpacity onPress={() => toggleDrawer()}>
-        <Image
-          source={{
-            uri:
-              "https://raw.githubusercontent.com/AboutReact/sampleresource/master/drawerWhite.png"
-          }}
-          style={{
-            width: 25,
-            height: 25,
-            marginLeft: 5
-          }}
-        />
-      </TouchableOpacity>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Feed!</Text>
     </View>
   );
-};
+}
 
-function landingScreenStack({ navigation }) {
+function Profile() {
   return (
-    <Stack.Navigator initialRouteName="LandingPage">
-      <Stack.Screen
-        name="LandingPage"
-        component={LandingPage}
-        options={{
-          title: "Beach Checker",
-          headerLeft: () => (
-            <NavigationDrawerStructure navigationProps={navigation} />
-          ),
-          headerStyle: {
-            backgroundColor: "deepskyblue"
-          },
-          headerTintColor: "#fff",
-          headerTitleStyle: {
-            fontWeight: "bold"
-          }
-        }}
-      />
-    </Stack.Navigator>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Profile!</Text>
+    </View>
   );
 }
 
-function faqScreenStack({ navigation }) {
+function Notifications() {
   return (
-    <Stack.Navigator initialRouteName="FaqPage">
-      <Stack.Screen
-        name="FaqPage"
-        component={FaqPage}
-        options={{
-          title: "Beach Checker",
-          headerLeft: () => (
-            <NavigationDrawerStructure navigationProps={navigation} />
-          ),
-          headerStyle: {
-            backgroundColor: "deepskyblue"
-          },
-          headerTintColor: "#fff",
-          headerTitleStyle: {
-            fontWeight: "bold"
-          }
-        }}
-      />
-    </Stack.Navigator>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Notifications!</Text>
+    </View>
   );
 }
 
-function App() {
+const Tab = createMaterialBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Feed"
+      activeColor="aliceblue"
+      labelStyle={{ fontSize: 12 }}
+      style={{ backgroundColor: 'aliceblue' }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Feed}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Notifications}
+        options={{
+          tabBarLabel: 'Search',
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="search" size={24} color={color} size={26}/>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Map"
+        component={Notifications}
+        options={{
+          tabBarLabel: 'Map',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="map-marker-circle" size={24} color={color} size={26}/>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Faq"
+        component={Notifications}
+        options={{
+          tabBarLabel: 'FAQ',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="frequently-asked-questions" size={24} color={color} size={26}/>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Feedback"
+        component={Profile}
+        options={{
+          tabBarLabel: 'Feedback',
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="feedback" color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator
-        drawerContentOptions={{
-          activeTintColor: "#e91e63",
-          itemStyle: { marginVertical: 5 }
-        }}
-      >
-        <Drawer.Screen
-          name="LandingPage"
-          options={{ drawerLabel: "Dashboard" }}
-          component={landingScreenStack}
-        />
-        <Drawer.Screen
-          name="FaqPage"
-          options={{ drawerLabel: "FAQ's" }}
-          component={faqScreenStack}
-        />
-      </Drawer.Navigator>
+      <MyTabs />
     </NavigationContainer>
   );
 }
-
-export default App;
