d3072f2dc3c3cef20d33bbb28193c15c0f58acc4
Sam Friggens <sam.friggens@adi.do>
2020-11-11 12:02:16 +0000

    ssdfawf

--- components/info/styles.js
+++ components/info/styles.js
@@ -7,37 +7,38 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
     paddingRight: 10,
     borderBottomColor: "rgba(158, 150, 150, .25)",
-    borderBottomWidth: 1,
+    borderBottomWidth: 1
   },
   textPadding: {
     paddingTop: 5,
-    paddingBottom: 5,
+    paddingBottom: 5
   },
   textNotice: {
     color: "red",
-    fontSize: 24,
+    fontSize: 24
   },
   textFaq: {
+    marginTop: 50,
     color: "black",
-    fontSize: 24,
+    fontSize: 24
   },
   congestionView: {
     flexDirection: "row",
     paddingRight: 75,
-    paddingLeft: 75,
+    paddingLeft: 75
   },
   congestionText: {
-    marginLeft: 15,
+    marginLeft: 15
   },
   congestionColour: {
-    backgroundColor: "white",
+    backgroundColor: "white"
   },
   noticeFlex: {
-    flex: 2,
+    flex: 2
   },
   helpFlex: {
-    flex: 3,
-  },
+    flex: 3
+  }
 });
 
 export default styles;
